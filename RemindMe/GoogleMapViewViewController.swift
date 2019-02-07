7c7accff03ad240ef4f74b804a070cf8ea75cecc
Alicelavander <alicelavander@gmail.com>
2019-02-07 11:41:44 +0900

    no message

---
+++ RemindMe/GoogleMapViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoogleMapViewViewController.swift
+//  RemindMe
+//
+//  Created by Alicelavander on 2018/12/22.
+//  Copyright © 2018年 Alicelavander. All rights reserved.
+//
+
+import UIKit
+
+class GoogleMapViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
