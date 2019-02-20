8bd098f33e6bd0ea4e3431b765731d86ccb37e20
Alicelavander <alicelavander@gmail.com>
2019-02-20 19:37:53 +0900

    できた気がする...!

---
+++ RemindMe/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//  RemindMe
+//
+//  Created by Alicelavander on 2019/02/20.
+//  Copyright © 2019年 Alicelavander. All rights reserved.
+//
+
+import UIKit
+
+class NotificationManager: NSObject {
+
+}
