599aa755297c1158439e6c13b6aa2eed059f3d68
Alicelavander <alicelavander@gmail.com>
2019-09-17 14:42:05 +0900

    でけた！

--- RemindMe/SettingViewController.swift
+++ RemindMe/SettingViewController.swift
@@ -15,14 +15,19 @@ class SettingViewController: UIViewController {
     
     func DeleteAll(_ sender: Any) {
         ToDoRepository.shared.removeAllTODO()
-        let alert: UIAlertController = UIAlertController(title: "タスク全削除完了", message: "全てのタスクを削除しました。", preferredStyle:  UIAlertController.Style.alert)
+        
+        
+        let alert: UIAlertController = UIAlertController(title: NSLocalizedString("titleAfter.removeALLTODO", comment: ""), message: NSLocalizedString("alartAfter.removeALLTODO", comment: ""), preferredStyle:  UIAlertController.Style.alert)
+        
+        
+        
         let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
         alert.addAction(defaultAction)
         present(alert, animated: true, completion: nil)
     }
     
     @IBAction func dispAlert(sender: UIButton) {
-        let alert: UIAlertController = UIAlertController(title: "タスク全削除", message: "全てのタスクを削除してもいいですか？今リストに表示されていないものも削除されます。", preferredStyle:  UIAlertController.Style.alert)
+        let alert: UIAlertController = UIAlertController(title: NSLocalizedString("titleBefore.removeALLTODO", comment: ""), message: NSLocalizedString("alartBefore.removeALLTODO", comment: ""), preferredStyle:  UIAlertController.Style.alert)
         let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
             (action: UIAlertAction!) -> Void in
             self.DeleteAll(self)
